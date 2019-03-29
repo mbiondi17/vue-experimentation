e9a002b19a89d7fb8307ec8093a1e9a87bb03d69
mbiondi17 <mvb5bg@virginia.edu>
2019-03-29 00:27:32 -0400

    Lessons 1 & 2

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 var product = "Socks"
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        product: "Socks",
+        description: "Something really cool, we promise!",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        link: "404"
+    }
+})
